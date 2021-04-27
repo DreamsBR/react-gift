0838aeed2e03d774c092eb4afa99f9940c64cc2a
Marcos Basurto <mbrlinc@hotmail.com>
2021-04-27 00:48:30 -0500

    aea

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifts = async( cateogy ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(cateogy)}&limit=10&api_key=RCvSBBYEc2Bb1oXdHiuhhoVCS5jn3SWx`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(cateogy)}&limit=10&api_key=RCvSBBYEc2Bb1oXdHiuhhoVCS5jn3SWx`
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map(img => {
